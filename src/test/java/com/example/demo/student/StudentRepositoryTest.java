a23bba8341f5c55fd38b51f051f71f4d48dd4404
Mo salah <mohumed.s.omar@gmail.com>
2021-06-02 10:19:21 +0200

    add test for add and getall

---
+++ src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.example.demo.student;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class StudentRepositoryTest {
+
+    @Test
+    void findStudentByEmail() {
+    }
+}
---
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -0,0 +1,23 @@
+package com.example.demo.student;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class StudentServiceTest {
+
+    @Test
+    void getStudent() {
+    }
+
+    @Test
+    void addNewStudent() {
+    }
+
+    @Test
+    void deleteStudent() {
+    }
+
+    @Test
+    void updateStudent() {
+    }
+}
